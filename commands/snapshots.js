506e60854706699dc4c0be1ea0c1cc19d9f59c34
max <mozg.mayhem@gmail.com>
2019-08-22 11:45:26 +0300

    Fix orthographic error

--- commands/snapshots.js
+++ commands/snapshots.js
@@ -2,6 +2,6 @@ const chalk = require('chalk')
 
 module.exports = {
   command: 'snapshots',
-  describe: chalk.green("manage an application's event store snapshots"),
+  describe: chalk.green("manage an application's event store's snapshots"),
   builder: yargs => yargs.commandDir('snapshots')
 }
